7208f25e9db7d35d176e4ba3b25ec9e1db7534f9
Hyf <57202807+hyf1530588058@users.noreply.github.com>
2024-08-19 17:42:02 +0800

    Update run_heteromorpheus_N.py

--- examples/run_heteromorpheus_N.py
+++ examples/run_heteromorpheus_N.py
@@ -2,19 +2,20 @@ import random
 import numpy as np
 import datetime
 
-from ga.run_universal4 import run_universal4
+from ga.run_ga_universal import run_meta
 
 if __name__ == "__main__":
     seed = 0
     random.seed(seed)
     np.random.seed(seed)
     
-    run_universal4(
+    run_meta(
         structure_shape = (5,5),
-        experiment_name = "test_heteromorpheus_n",
-        max_evaluations = 12,
+        experiment_name = "Train_SOAT",
+        max_evaluations = 100,
+        pop_size = 10
         train_iters = 1000,
-        num_cores = 12
+        num_cores = 10
     )
 
     print('run_universal over at ', datetime.datetime.now())
